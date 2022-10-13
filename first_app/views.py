3bb62276c79bf3ce448cdb52d4aea20832620957
chintan7786 <davdachintan97@gmail.com>
2022-10-13 13:07:10 +0530

    user authentication

--- first_app/views.py
+++ first_app/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
+from pkg_resources import require
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 
 
+class UserCheck(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Success'}
+        return Response(content)
+
+
+
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def index(request):
-    return Response({
-        'status': 200,
-        'msg' : 'yes working'
-    })
+    if request.method == 'GET':
+        return Response({
+            'status': 200,
+            'msg' : 'yes working',
+            'method' : 'GET'
+        })
+    elif request.method == 'POST':
+        return Response({
+            'method': 'POST'
+        })
+    else:
+        return Response({
+            'method': 'invalid'
+        })
